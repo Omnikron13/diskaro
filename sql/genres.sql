26aca7a1e6386d6f59a6645e060daf27a603248e
Joey Sabey <GameFreak7744@gmail.com>
2013-12-30 21:57:03 +0000

    Added a unique index to the subGenres table to prevent duplicate links

--- sql/genres.sql
+++ sql/genres.sql
@@ -17,3 +17,6 @@ CREATE TABLE IF NOT EXISTS subGenres ( --name..?
     FOREIGN KEY (parentID) REFERENCES genres (id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (childID) REFERENCES genres (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+--Index to ensure there are no duplicate sub-genre links
+CREATE UNIQUE INDEX IF NOT EXISTS subGenresIndex ON subGenres (parentID, childID);
