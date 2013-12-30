a5f226495c21f86d0d455f523bcf62acbec356d0
Joey Sabey <GameFreak7744@gmail.com>
2013-12-30 21:56:18 +0000

    Fixed a missing comma in the release table schema

--- sql/releases.sql
+++ sql/releases.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS releases (
     name TEXT NOT NULL, --null perhaps allowed for untitled..?
     --perhaps subtitle field?
 --    artistID INTEGER NOT NULL, --and what of compilations or dual releases?
-    year INTEGER --perhaps nulls allowed where incomplete/unknown. date instead?
+    year INTEGER, --perhaps nulls allowed where incomplete/unknown. date instead?
     labelID INTEGER,
     FOREIGN KEY (labelID) REFERENCES labels (id) ON DELETE SET NULL ON UPDATE CASCADE
 );
