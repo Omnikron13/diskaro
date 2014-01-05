4cf5c5518462ef22c62534dedd9b6e76ed60e0f7
Joey Sabey <GameFreak7744@gmail.com>
2014-01-05 22:20:13 +0000

    Added a subLabels table & subLabelsIndex index to the schema

--- sql/labels.sql
+++ sql/labels.sql
@@ -5,3 +5,18 @@ CREATE TABLE IF NOT EXISTS labels (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL
 );
+
+/*====================*
+| Relationships Table |
+*====================*/
+CREATE TABLE IF NOT EXISTS subLabels (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    parentID INTEGER NOT NULL,
+    childID INTEGER NOT NULL, --should this be unique? or multiple parents allowed?
+    CHECK (parentID != childID),
+    FOREIGN KEY (parentID) REFERENCES labels (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (childID) REFERENCES labels (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--Index to ensure there are no duplicate sub-genre links
+CREATE UNIQUE INDEX IF NOT EXISTS subLabelsIndex ON subLabels (parentID, childID);
