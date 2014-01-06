7e5d70cc80ae4046d3c7de92691484285ef3f66d
Joey Sabey <GameFreak7744@gmail.com>
2014-01-06 21:35:49 +0000

    Refactored the Artist class stub to now descend from TagCore

--- src/com/googlecode/diskaro/library/Artist.java
+++ src/com/googlecode/diskaro/library/Artist.java
@@ -1,8 +1,31 @@
 package com.googlecode.diskaro.library;
 
-import java.util.*;
+import java.sql.SQLException;
 
-public class Artist {
-	protected int id;
-	protected String name;
+public class Artist extends TagCore {
+	protected String getTable() {
+		return "artists";
+	}
+	protected String getRelationshipTable() {
+		return "artistPseudonyms";
+	}
+	
+	/**
+	 * Loads an artist from the database by a specified uid
+	 * @param id the internal uid
+	 * @throws SQLException
+	 */
+	public Artist(int id) throws SQLException {
+		super(id);
+	}
+	
+	/**
+	 * Adds a new artist entry to the database, and returns a corresponding Artist object
+	 * that can be used to manipulate the new artist.
+	 * @param name name of the new artist to be created
+	 * @return Artist object representing the newly added artist
+	 */
+	public static Artist add(String name) throws SQLException {
+		return new Artist(add(name, "artists"));
+	}
 }
