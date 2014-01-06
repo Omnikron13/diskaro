215b03c8e624d66da7b3082ca3c8d851b3ae50b9
Joey Sabey <GameFreak7744@gmail.com>
2014-01-06 19:34:22 +0000

    Removed name based constructor from Release - release names are not unique!

--- src/com/googlecode/diskaro/library/Release.java
+++ src/com/googlecode/diskaro/library/Release.java
@@ -35,22 +35,6 @@ public class Release extends DataCore {
 		}
 	}
 	
-	public Release(String name) throws SQLException {
-		PreparedStatement ps = Library.getDB().prepareStatement("SELECT * FROM releases WHERE name = ?;");
-		ps.setString(1, name);
-		ResultSet rs = ps.executeQuery();
-		this.id = rs.getInt("id");
-		this.name = rs.getString("name");
-		int year = rs.getInt("year");
-		if(!rs.wasNull()) {
-			this.year = year;
-		}
-		int labelID = rs.getInt("labelID");
-		if(!rs.wasNull()) {
-			this.label = new Label(labelID);
-		}
-	}
-	
 	public static Release add(String name, Integer year, Label label) throws SQLException {
 		PreparedStatement ps = Library.getDB().prepareStatement("INSERT INTO releases (name, year, labelID) VALUES (?, ?, ?);");
 		ps.setString(1, name);
