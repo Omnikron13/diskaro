60075f28d8becc75109c98914af33ff8667edbdb
Joey Sabey <GameFreak7744@gmail.com>
2014-01-06 21:41:16 +0000

    Updated the artist (& related) table schema to use parentID & childID instead of canonicalID & pseudonymID to allow it to work correctly with the TagCore codebase

--- sql/artists.sql
+++ sql/artists.sql
@@ -13,12 +13,12 @@ CREATE TABLE IF NOT EXISTS artists (
 *==============================================*/
 CREATE TABLE IF NOT EXISTS artistPseudonyms (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    canonicalID INTEGER NOT NULL,
-    pseudonymID INTEGER UNIQUE NOT NULL,
-    CHECK (canonicalID != pseudonymID),
-    FOREIGN KEY (canonicalID) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (pseudonymID) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE
+    parentID INTEGER NOT NULL,
+    childID INTEGER UNIQUE NOT NULL,
+    CHECK (parentID != childID),
+    FOREIGN KEY (parentID) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (childID) REFERENCES artists (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 --unique index to prevent duplicate pseudonym links
-CREATE UNIQUE INDEX IF NOT EXISTS artistPseudonymIndex ON artistPseudonyms (canonicalID, pseudonymID);
+CREATE UNIQUE INDEX IF NOT EXISTS artistPseudonymIndex ON artistPseudonyms (parentID, childID);
