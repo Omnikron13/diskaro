2d02f74bb5b36ed6299000f04b06432628121031
Joey Sabey <GameFreak7744@gmail.com>
2014-01-06 22:54:56 +0000

    Added a comment explaining the basic (non static) class variables

--- src/com/googlecode/diskaro/library/Track.java
+++ src/com/googlecode/diskaro/library/Track.java
@@ -14,6 +14,7 @@ import java.sql.Statement;
  *
  */
 public class Track extends DataCore {
+	//Basic information about a track
 	protected File path;
 	protected Artist artist = null;		//Primary/main artist
 	protected Release release = null;
