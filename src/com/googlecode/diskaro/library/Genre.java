0de22e77e363b1a504a273ca7edab8c063f55ffa
Joey Sabey <GameFreak7744@gmail.com>
2014-01-07 21:33:55 +0000

    Added Genre.table() method as the single point of access for the primary table name, and updated Genre to use it internally

--- src/com/googlecode/diskaro/library/Genre.java
+++ src/com/googlecode/diskaro/library/Genre.java
@@ -3,8 +3,16 @@ package com.googlecode.diskaro.library;
 import java.sql.*;
 
 public class Genre extends TagCore {
-	protected String getTable() {
+	/**
+	 * Returns the name of the primary table used to store genres.
+	 * This is primarily so methods such as DataCore.get() can be used.
+	 * @return primary table name - "genres"
+	 */
+	public static String table() {
 		return "genres";
+	}
+	protected String getTable() {
+		return table();
 	}
 	protected String getRelationshipTable() {
 		return "subGenres";
@@ -22,6 +30,6 @@ public class Genre extends TagCore {
 	 * Adds a new genre entry to the database
 	 */
 	public static Genre add(String name) throws SQLException {
-		return new Genre(add(name, "genres"));
+		return new Genre(add(name, table()));
 	}
 }
