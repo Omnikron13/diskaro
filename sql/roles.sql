8913975d7557c6a3db762e637fd03f1f3ebe3fcc
Joey Sabey <GameFreak7744@gmail.com>
2014-06-28 20:36:49 +0100

    Added sql/roles.sql - schema for roles & subRoles tables for holding information about different artist roles (e.g. producer, recording artist, remixer, etc.) to be used when tagging tracks

---
+++ sql/roles.sql
@@ -0,0 +1,22 @@
+/*===================*
+| Artist roles table |
+*===================*/
+CREATE TABLE IF NOT EXISTS roles (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+/*====================*
+| Relationships Table |
+*====================*/
+CREATE TABLE IF NOT EXISTS subRoles (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    parentID INTEGER NOT NULL,
+    childID INTEGER UNIQUE NOT NULL,
+    CHECK (parentID != childID),
+    FOREIGN KEY (parentID) REFERENCES roles (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (childID) REFERENCES roles (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+--unique index to prevent duplicate subrole links
+CREATE UNIQUE INDEX IF NOT EXISTS subRolesIndex ON subRoles (parentID, childID);
