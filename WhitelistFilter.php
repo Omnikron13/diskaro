2f33d348da91424b597b94be84aa88d7271b4ed8
Joey Sabey <GameFreak7744@gmail.com>
2014-06-28 19:10:34 +0100

    Updated WhitelistFilter to utilise the new ListFilter abstract class

--- WhitelistFilter.php
+++ WhitelistFilter.php
@@ -1,50 +1,14 @@
 <?php
 
-require_once('Track.php');
+require_once('ListFilter.php');
 
-abstract class WhitelistFilter {
-    protected $list = [];
-    protected $recursive = false;
-
-    public function __construct($items, $recursive = false) {
-        if(is_array($items))
-            $this->list = $items;
-        else
-            $this->list[] = $items;
-        $this->recursive = $recursive;
+abstract class WhitelistFilter extends ListFilter {
+    protected static function check($item, $track) {
+        return in_array($item, static::getTags($track));
     }
-
-    public function __invoke($track) {
-        foreach($this->getList() as $i) {
-            if(in_array($i, static::getTags($track)))
-                return true;
-        }
-        return false;
+    protected static function match() {
+        return true;
     }
-
-    public function add($item) {
-        if(in_array($item, $this->list))
-            throw new Exception('Item already in list');
-        $this->list[] = $item;
-    }
-
-    public function remove($item) {
-        unset($this->list[array_search($item, $this->list)]);
-        $this->list = array_values($this->list);
-    }
-
-    public function getList() {
-        if(!$this->recursive)
-            return $this->list;
-        $list = $this->list;
-        foreach($list as $i) {
-            $list = array_unique(array_merge($list, $i->getChildren(true)));
-        }
-        return $list;
-    }
-
-    //Should return an array of items to check the list against
-    protected abstract static function getTags($track);
 }
 
 ?>
