fef3b873a068b1c88832255052ea4a864e6d3f3d
Joey Sabey <GameFreak7744@gmail.com>
2014-06-30 19:41:51 +0100

    Removed NOT NULL constraint from tracks->path field in the schema so NULL paths can be used for missing files, etc.

--- sql/tracks.sql
+++ sql/tracks.sql
@@ -5,7 +5,7 @@
 CREATE TABLE IF NOT EXISTS tracks (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL, --maybe null for untitled?
-    path TEXT NOT NULL UNIQUE,
+    path TEXT UNIQUE,
     --possible subtitle column?
     artistID INTEGER,-- NOT NULL, --what about unknown/incomplete? also, remixers/etc?
     releaseID INTEGER,-- NOT NULL, --ditto ^
