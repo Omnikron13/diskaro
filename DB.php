3622c962575f7cb58e2799f4784fc7eebf1d970a
Joey Sabey <GameFreak7744@gmail.com>
2014-07-02 20:07:07 +0100

    Updated DB::get() so the PDO object is (re)created if it is NULL /or/ the file specified by DB::PATH doesn't exist

--- DB.php
+++ DB.php
@@ -6,7 +6,7 @@ class DB {
     protected static $db = NULL;
 
     public static function get() {
-		if(self::$db === NULL) {
+		if(self::$db === NULL || !file_exists(static::PATH)) {
 			self::$db = new PDO('sqlite:'.self::PATH);
 			self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			self::$db->exec('PRAGMA foreign_keys = ON');
