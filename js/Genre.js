b3effecea0f6b6ec3f5e1029f15f4c7da97d1f9a
Joey Sabey <GameFreak7744@gmail.com>
2014-07-16 19:35:23 +0100

    Added Genre.load() static method - pulls (optionally filtered) genre data from the DB and passes an array of Genre objects to the callback passed to it

--- js/Genre.js
+++ js/Genre.js
@@ -6,3 +6,15 @@ function Genre(json) {
     this.name = json.name;
     this.parentIDs = json.parentIDs;
 };
+
+//Static method which requests an (optionally filtered) list of genres from
+//the DB and passes a list of Genre objects to the provided callback
+Genre.load = function(cb, f) {
+    return new Request('Genre', f?f.constraint():null)
+        .pull(function(json) {
+            cb(json.map(function(g) {
+                return new Genre(g);
+            }));
+        })
+    ;
+};
