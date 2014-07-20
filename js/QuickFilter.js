36317e773947d68f3522665ecfa413098afea239
Joey Sabey <GameFreak7744@gmail.com>
2014-07-20 22:23:59 +0100

    Added .Regex() static method to QuickFilter - generates a QuickFilter element that passes RegexFilter objects to its callback

--- js/QuickFilter.js
+++ js/QuickFilter.js
@@ -36,6 +36,44 @@ QuickFilter.data = function(type, cb) {
     return qf;
 };
 
+//Static method for generating regex QuickFilter elements
+QuickFilter.Regex = function(cb) {
+    var qf = new QuickFilter('Quick Regex Filter');
+    qf
+        .append(
+            $('<form>')
+                .append(
+                    $('<label>')
+                        .attr('for', 'caseField')
+                        .html('Case Sensitive')
+                )
+                .append(
+                    $('<input>')
+                        .attr('name', 'caseField')
+                        .attr('type', 'checkbox')
+                )
+                .append(
+                    $('<input>')
+                        .attr('name', 'regexField')
+                        .attr('type', 'search')
+                        .attr('placeholder', 'Enter regex...')
+                )
+                .append(
+                    $('<button>')
+                        .attr('type', 'button')
+                        .html('Filter')
+                        .on('click', function() {
+                            cb(Filter.Regex(
+                                '/' + qf.find('[name="regexField"]').val() + '/'
+                                + (qf.find('[name="caseField"]').is(':checked')?'':'i')
+                            ));
+                        })
+                )
+        )
+    ;
+    return qf;
+};
+
 //Shorthand static methods for creating some simple QuickFilters
 QuickFilter.Genre = function(cb) {
     return QuickFilter.data('Genre', cb);
