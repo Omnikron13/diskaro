470b918da6d2df127026620103dff8b9407d9300
Joey Sabey <GameFreak7744@gmail.com>
2014-07-21 18:30:42 +0100

    Added QuickFilter.UI.js - defines new 'namespace' QuickFilter.UI to separate out functionality which depends on jQuery UI. Currently holds a single 'subnamespace' (QuickFilter.UI.Tabs) for working with jQuery UI Tabs widgets which contains the single function .render() which renders out a Tabs widget from an array of QuickFilter jQuery elements, and a .mainTag static property which defines which HTML tag the main Tabs container renders as (defaulting to '<section>')

---
+++ js/QuickFilter.UI.js
@@ -0,0 +1,33 @@
+//require_once(QuickFilter.js)
+
+//UI 'namespace' for jQuery UI functionality
+QuickFilter.UI = {
+    //Subnamespace for Tabs widget functionality
+    Tabs: {
+        //Determines which HTML tag the Tabs container should be
+        mainTag: '<section>',
+
+        //Function to render Tabs widget from an array of QuickFilters
+        render: function(qfs) {
+            return $(QuickFilter.UI.Tabs.mainTag)
+                .addClass('quickFilterTabs')
+                .append(
+                    $('<ul>')
+                        .append(
+                            qfs.map(function(qf) {
+                                return $('<li>')
+                                    .append(
+                                        $('<a>')
+                                            .attr('href', '#'+qf.attr('id'))
+                                            .html(qf.find('.quickFilterName').html())
+                                    )
+                                ;
+                            })
+                        )
+                )
+                .append(qfs)
+                .tabs()
+            ;
+        }
+    }
+};
