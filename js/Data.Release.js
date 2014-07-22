6ebf4c0cedb61f3b4a31ee862af1816d27b220b9
Joey Sabey <GameFreak7744@gmail.com>
2014-07-22 21:24:54 +0100

    Updated Data.Release() to use Data.Label() instead of 'new Label()' when setting its .label property (and added a require on Data.Label.js)

--- js/Data.Release.js
+++ js/Data.Release.js
@@ -1,10 +1,11 @@
 //require_once(Data.js)
+//require_once(Data.Label.js)
 
 //Pseudo-constructor for creating release Data objects
 Data.Release = function(json) {
     var r = new Data('Release', json);
     r.year = json.year;
-    r.label = json.label===null?null:new Label(json.label);
+    r.label = json.label===null?null:Data.Label(json.label);
     return r;
 };
 
