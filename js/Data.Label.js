9c097f0ad85dec52bf21957286d6d875787675e7
Joey Sabey <GameFreak7744@gmail.com>
2014-07-22 21:15:35 +0100

    Added Data.Label.js - essentially the same as Data.Genre but for label Data objects. Notably shares .parentIDs (as anything deriving from the PHP SubDataCore class does) so this should probably be abstracted into Data

---
+++ js/Data.Label.js
@@ -0,0 +1,13 @@
+//require_once(Data.js)
+
+//Pseudo-constructor for creating label Data objects
+Data.Label = function(json) {
+    var l = new Data('Label', json);
+    l.parentIDs = json.parentIDs;
+    return l;
+};
+
+//Shorthand for using Data.load() to load labels
+Data.Label.load = function(cb, f) {
+    return Data.load('Label', cb, f);
+};
