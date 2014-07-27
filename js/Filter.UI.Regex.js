fa0441d3b5aa11bbebb56d4501ae23000f677d98
Joey Sabey <GameFreak7744@gmail.com>
2014-07-27 21:43:13 +0100

    Added Filter.UI.Regex.js with new Filter.UI.Regex namespace - for working with RegexFilter/Filter.Regex UI elements

---
+++ js/Filter.UI.Regex.js
@@ -0,0 +1,52 @@
+//require_once(Filter.UI.js)
+
+//Namespace for regex filter UI elements
+Filter.UI.Regex = {
+    //Function for creating RegexFilter/Filter.Regex UI elements
+    render: function(prefix) {
+        //Create element from base template
+        var e = Filter.UI.core.render(
+            'Regex',
+            Filter.UI.core.optionCheckbox(prefix, 'CaseSensitive', 'Case Sensitive'),
+            prefix
+        )
+            //Add regex specific UI fields/controls
+            .append(
+                $('<fieldset>')
+                    .append(
+                        $('<legend>')
+                            .html('Regex')
+                    )
+                    .append(
+                        $('<label>')
+                            .attr('for', 'regexField')
+                            .html('Regex')
+                    )
+                    .append(
+                        $('<input>')
+                            .addClass('regex')
+                            .attr('name', 'regexField')
+                            .attr('type', 'text')
+                            .attr('placeholder', 'Enter regex...')
+                    )
+            )
+        ;
+
+        //Method to generate the PCRE/PHP regex str
+        e.getRegex = function() {
+            return '/' + e.find('.regex').val() + '/'
+                + (e.getOption('CaseSensitive')?'':'i')
+            ;
+        };
+
+        //Method to return a Filter object based on options/entered regex
+        e.getFilter = function() {
+            return Filter.Regex(
+                e.getRegex(),
+                e.getOption('Negate')
+            );
+        };
+
+        return e;
+    }
+};
