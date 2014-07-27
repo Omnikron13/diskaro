a69140e66b23067b94069bc52c58a4a3746662cc
Joey Sabey <GameFreak7744@gmail.com>
2014-07-27 21:39:33 +0100

    Moved Filter.UI.Data namespace into Filter.UI.Data.js (from Filter.UI.js)

--- js/Filter.UI.js
+++ js/Filter.UI.js
@@ -62,44 +62,5 @@ Filter.UI = {
                 )
             ;
         }
-    },
-
-    //Subnamespace for Data[List] Filter UI elements (e.g. GenreFilter)
-    Data: {
-        //Function for creating Data[List] UI elements of arbitrary type
-        render: function(dl, prefix) {
-            //Create element from base template
-            var e = Filter.UI.core.render(
-                dl.type,
-                Filter.UI.core.optionCheckbox(prefix, 'Recursive'),
-                prefix
-            )
-                //Add generic filter type class
-                .addClass('dataFilter')
-                //Add main DataList selection element
-                .append(
-                    DataList.UI.Buttonset.render(
-                        dl,
-                        prefix
-                    )
-                )
-            ;
-            //Add method to check recursive option
-            e.isRecursive = function() {
-                return e.find('#' + prefix + dl.type + 'FilterRecursive')
-                    .is(':checked');
-            };
-            //Add method to get Filter object
-            e.getFilter = function() {
-                var id = e
-                    .find('.dataButtonset :radio:checked')
-                        .val()
-                ;
-                return typeof id === 'undefined'?
-                    null:
-                    Filter[dl.type]({id: id}, e.getOption('Recursive'), e.getOption('Negate'));
-            };
-            return e;
-        }
     }
 };
