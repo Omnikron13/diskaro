17b0a81371226cf0b69528488cff0684a2c6834e
Joey Sabey <GameFreak7744@gmail.com>
2014-08-05 20:32:45 +0100

    Added .renderSearch() method to Filter.UI.Data - renders a search section (<fieldset>) element containing a search box (search type input tag) with an on input event handler which filters (.show()/.hide()s) the data buttons of its parent .dataFilter by an entered regex search string

--- js/Filter.UI.Data.js
+++ js/Filter.UI.Data.js
@@ -12,6 +12,8 @@ Filter.UI.Data = {
         )
             //Add generic filter type class
             .addClass('dataFilter')
+            //Add search section element
+            .append(Filter.UI.Data.renderSearch())
             //Add main DataList selection element
             .append(
                 DataList.UI.Buttonset.render(
@@ -50,5 +52,47 @@ Filter.UI.Data = {
                 Filter[dl.type]({id: id}, e.getOption('Recursive'), e.getOption('Negate'));
         };
         return e;
+    },
+
+    //Render a search section/field for filtering the DataList.UI Buttons
+    renderSearch: function() {
+        return $('<fieldset>')
+            //Add class for generic selection (e.g. .dataFilter>.search)
+            .addClass('search')
+            //Render legend/header
+            .append(
+                $('<legend>')
+                    .html('Search')
+            )
+            //Render field label
+            .append(
+                $('<label>')
+                    .attr('for', 'search')
+                    .html('Search')
+            )
+            //Render actual search box/field
+            .append(
+                $('<input>')
+                    .attr('type', 'search')
+                    .attr('name', 'search')
+                    .attr('placeholder', 'Search...')
+                    //Show/Hide DataList Buttons on search string changes
+                    .on('input', function() {
+                        //Create regex from entered string
+                        var regex = new RegExp($(this).val(), 'i');
+                        //Find button elements & iterate
+                        $(this).parents('.dataFilter')
+                            .find('.body.buttons>.dataRadio')
+                                .each(function() {
+                                    //Show/Hide based on regex match
+                                    if(regex.test($(this).find('.ui-button-text').html()))
+                                        $(this).show();
+                                    else
+                                        $(this).hide();
+                                })
+                        ;
+                    })
+            )
+        ;
     },
 };
