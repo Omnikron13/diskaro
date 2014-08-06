c13caa8ed3d27700959b7aada996505f82cf7aeb
Joey Sabey <GameFreak7744@gmail.com>
2014-08-06 20:30:50 +0100

    Added locale/locale_template.js - contains a (probably incomplete) template of translation string overrides

---
+++ js/locale/locale_template.js
@@ -0,0 +1,38 @@
+//Localisation string overrides template
+$.extend(_._strings, {
+    //Data types/classes
+    'Artist':   '',
+    'Artists':  '',
+    'Genre':    '',
+    'Genres':   '',
+    'Label':    '',
+    'Labels':   '',
+    'Release':  '',
+    'Releases': '',
+    'Role':     '',
+    'Roles':    '',
+    'Tag':      '',
+    'Tags':     '',
+    //TrackList/TrackColumn headings (excluding Data type/class)
+    '#':     '',
+    'Title': '',
+    //Filter[.UI] strings
+    'Negate'      : '',
+    'QuickFilter' : '',
+        //Types (excluding Data type/class)
+        'Regex'    : '',
+        'Compound' : '',
+        //Data
+        'Recursive': '',
+        //Regex
+        'Case Sensitive' : '',
+        'Enter regex'    : '',
+        //Compound
+        'New Filter'   : '',
+        'Empty Filter' : '',
+    //Misc.
+    'Search':         '',
+    'Shuffle':        '',
+    'Shuffle Off':    '',
+    'Shuffle On':     '',
+});
