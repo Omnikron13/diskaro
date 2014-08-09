e035b6d8fe143d7cd0b59cb9a829bee3c635fd5b
Joey Sabey <GameFreak7744@gmail.com>
2014-08-09 21:09:05 +0100

    Updated js/locale/locale_template.js to list all (?) the localisation strings currently used

--- js/locale/locale_template.js
+++ js/locale/locale_template.js
@@ -24,22 +24,52 @@ $.extend(_._strings, {
     '#':     '',
     'Title': '',
     //Filter[.UI] strings
-    'Negate'      : '',
+    'Filter'      : '',
+    'Filters'     : '',
     'QuickFilter' : '',
-        //Types (excluding Data type/class)
+    'Options'     : '',
+    'Negate'      : '',
+        //Long type strings
+        'Artist Filter'   : '',
+        'Genre Filter'    : '',
+        'Label Filter'    : '',
+        'Release Filter'  : '',
+        'Role Filter'     : '',
+        'Tag Filter'      : '',
+        'Regex Filter'    : '',
+        'Compound Filter' : '',
+        //Short type strings (excluding Data type/class)
         'Regex'    : '',
         'Compound' : '',
         //Data
         'Recursive': '',
+            //Placeholder text
+            'Artist list loading'  : '',
+            'Genre list loading'   : '',
+            'Label list loading'   : '',
+            'Release list loading' : '',
+            'Role list loading'    : '',
+            'Tag list loading'     : '',
         //Regex
         'Case Sensitive' : '',
         'Enter regex'    : '',
         //Compound
+        'Operator'     : '',
+        'Or'           : '',
+        'And'          : '',
+        'Xor'          : '',
         'New Filter'   : '',
         'Empty Filter' : '',
+    //Some generic predefined Role types
+    'Producer' : '',
+    'Remixer'  : '',
+    'Vocals'   : '',
     //Misc.
-    'Search':         '',
-    'Shuffle':        '',
-    'Shuffle Off':    '',
-    'Shuffle On':     '',
+    'Unknown'     : '',
+    'Loading'     : '',
+    'Remove'      : '',
+    'Search'      : '',
+    'Shuffle'     : '',
+    'Shuffle Off' : '',
+    'Shuffle On'  : '',
 });
