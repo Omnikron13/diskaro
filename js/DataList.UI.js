ce797d1e7c51d7b57b71b69bab8348d59e569654
Joey Sabey <GameFreak7744@gmail.com>
2014-08-12 21:33:16 +0100

    Added .Dialogue (sub)namespace to DataList.UI - functionality for creating (modal) jQuery UI Dialog widgets that contain an arbitrary DataList.UI.Buttonset element, prompting the user to choose an item (Data obj) from the list

--- js/DataList.UI.js
+++ js/DataList.UI.js
@@ -51,4 +51,77 @@ DataList.UI = {
             ;
         }
     },
+
+    //Subnamespace for creating UI dialogue boxes for users to select from a DataList
+    Dialogue: {
+        //Default main tag type
+        mainTag: '<div>',
+
+        //Default UI strings
+        saveStr  : _('Save'),
+        cancelStr: _('Cancel'),
+
+        //Main render function for creating actual Dialog UI widget
+        render: function(dl, prefix, options) {
+            //Set defaults
+            var mainTag   = DataList.UI.Dialogue.mainTag;
+            var saveStr   = DataList.UI.Dialogue.saveStr;
+            var cancelStr = DataList.UI.Dialogue.cancelStr;
+            //Check for option overrides
+            if(options) {
+                if(options.mainTag)   mainTag   = options.mainTag;
+                if(options.saveStr)   saveStr   = options.saveStr;
+                if(options.cancelStr) cancelStr = options.cancelStr;
+            }
+            //Render element
+            return $(mainTag)
+                //Add generic selection class
+                .addClass('dataDialogue')
+                //Add specific selection class
+                .addClass(dl.type.toLowerCase() + 'Dialogue')
+                //Set Dialog widget title (displays in top bar)
+                .attr('title', _('Select ' + dl.type))
+                //Add Buttonset for actual selection
+                .append(
+                    DataList.UI.Buttonset.render(dl, prefix)
+                        //Catch button selection & save Data obj
+                        .on('change', function(ev) {
+                            $(this).parent().data('data', $(this).data('data'));
+                        })
+                        //Target all Data Buttons
+                        .find('.dataRadio')
+                            //Add doubleclick-to-select functionality
+                            .on('dblclick', function() {
+                                $(this).parents('.dataDialogue')
+                                    .trigger('saveAndClose', $(this).data('data'))
+                            })
+                            .end()
+                )
+                //Custom (internal) event for shorthand save & close/destroy
+                .on('saveAndClose', function(ev, d) {
+                    $(this)
+                        .trigger('save', d)
+                        .dialog('destroy')
+                    ;
+                })
+                //Create Dialog widget
+                .dialog({
+                    width: 'auto',
+                    modal: true,
+                    resizable: false,
+                    draggable: false,
+                    buttons: [
+                        //Cancel: close/destroy without saving selection
+                        {text: cancelStr, click: function() {
+                            $(this).dialog('destroy');
+                        }},
+                        //Save: pass out selected Data & close/destroy
+                        {text: saveStr, click: function() {
+                            $(this).trigger('saveAndClose', $(this).data('data'));
+                        }},
+                    ],
+                })
+            ;
+        },
+    },
 };
