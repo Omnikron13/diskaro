78e47c98cd4636e14643a81bdfcb110567bc036b
Joey Sabey <GameFreak7744@gmail.com>
2014-08-17 20:37:43 +0100

    Added js/Data.UI.js which defines new Data.UI namespace for UI output of Data objects - currently consists of just a .LI subnamespace for rendering list item (<li>) elements (with .LI.render() function) from Data objects, which is intended to replace Data.renderLI() (to seperate UI/logic)

---
+++ js/Data.UI.js
@@ -0,0 +1,19 @@
+//require_once(Data.js)
+
+//Namespace for UI output/functionality
+Data.UI = {
+    //Subnamespace for ul/ol item (<li>) output
+    LI: {
+        //Function to render <li> (jQuery) element from Data obj
+        render: function(d) {
+            return $('<li>')
+                //Add generic selection class
+                .addClass('dataItem')
+                //Add specific selecion class (e.g. .dataItem.Genre)
+                .addClass(d.type)
+                //Render output text/html
+                .html(d.name)
+            ;
+        },
+    },
+},
