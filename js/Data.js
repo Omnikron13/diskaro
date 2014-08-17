792ca564d6db05a84fc4e835f336bae22acd160c
Joey Sabey <GameFreak7744@gmail.com>
2014-08-17 22:08:30 +0100

    Removed .renderRadio() method from Data - replaced by Data.UI.Radio (seperation of ui/logic)

--- js/Data.js
+++ js/Data.js
@@ -21,44 +21,6 @@ Data.method('toJSON', function() {
     return j;
 });
 
-//Render the Data as a radio button wrapped in a span, using the optional
-//prefix param to disambiguate id attributes if multiple buttons based on the
-//same Data object are needed
-Data.method('renderRadio', function(prefix) {
-    //Set default values
-    prefix = prefix || '';
-    //Construct id/for attr string
-    var idStr = prefix + this.type + 'Radio-' + this.id;
-    //Render element
-    return $('<span>')
-        //Add class for generic selection
-        .addClass('dataRadio')
-        .addClass(this.type.toLowerCase()+'Radio')
-        //Store Data obj in element
-        .data('data', this)
-        //Render button
-        .append(
-            $('<input>')
-                .attr('id', idStr)
-                .attr('type', 'radio')
-                .attr('name', prefix + this.type)
-                .val(this.id)
-                //Catch input change & retarget it to the main span (which
-                //will allow cleaner .data('data') access downstream)
-                .on('change', function(ev) {
-                    ev.stopPropagation();
-                    $(this).parent().trigger('change');
-                })
-        )
-        //Render label
-        .append(
-            $('<label>')
-                .attr('for', idStr)
-                .html(this.name)
-        )
-    ;
-});
-
 //Static method which requests an (optionally filtered) list of 'type' data
 //from the DB and passes a list of Data objects to the provided callback
 Data.load = function(type, cb, f) {
