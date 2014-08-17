110f4ce15676d97e6540ac78aa4f1a8dd001a55a
Joey Sabey <GameFreak7744@gmail.com>
2014-08-17 21:53:06 +0100

    Added Data.UI.Radio namespace - functionality for rendering/creating radio button elements from Data objects (for e.g. Data Filter UI, Track editting, etc.) to replace Data's .renderRadio() method (properly seperating logic/UI)

--- js/Data.UI.js
+++ js/Data.UI.js
@@ -18,4 +18,44 @@ Data.UI = {
             ;
         },
     },
+
+    //Subnamespace for creating radio button UI elements from Data obj
+    Radio: {
+        //Function to render <span> holding button/label pair
+        render: function(d, prefix) {
+            //Construct id/for attr string
+            var idStr = prefix + d.type + 'Radio-' + d.id;
+            return $('<span>')
+                //Store original Data obj
+                .data('data', d)
+                //Add generic selection class
+                .addClass('dataRadio')
+                //Add specific selecion class (e.g. .dataRadio.Genre)
+                .addClass(d.type)
+                //Render control/button
+                .append(
+                    $('<input>')
+                        //jQuery Buttonset requires id
+                        .attr('id', idStr)
+                        //Render as radio button
+                        .attr('type', 'radio')
+                        //Unique group name
+                        .attr('name', prefix + d.type)
+                        //Probably never needed, but store Data uid
+                        .val(d.id)
+                )
+                //Render label
+                .append(
+                    $('<label>')
+                        .attr('for', idStr)
+                        .html(d.name)
+                )
+                //Catch change events from actual input/radio element
+                .on('change', function() {
+                    //Retrigger custom event on main element (passing selected Data obj)
+                    $(this).trigger('dataSelect', $(this).data('data'));
+                })
+            ;
+        },
+    },
 },
