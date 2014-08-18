d677de07056529cbca11b416be40a520f836a466
Andy.kuli <kulikova.adriana@gmail.com>
2014-08-18 03:14:49 +0100

    added slovak translation file and completed the 1st section

---
+++ js/locale/locale-sk.js
@@ -0,0 +1,79 @@
+//Localisation string overrides template
+$.extend(_._strings, {
+    //Data types/classes
+    'Artist':   'Umelec',
+    'Artists':  'Umelci',
+    'Genre':    'Žáner',
+    'Genres':   'Žánre',
+    'Label':    'Vydavateľstvo',
+    'Labels':   'Vydavateľstvá',
+    'Release':  'Album',
+    'Releases': 'Albumy',
+    'Role':     'Rola',
+    'Roles':    'Role',
+    'Tag':      'Značka',
+    'Tags':     'Značky',
+    //DataList.UI default legend/header
+    /*
+    'Select Artist'  : '',
+    'Select Genre'   : '',
+    'Select Label'   : '',
+    'Select Release' : '',
+    'Select Role'    : '',
+    'Select Tag'     : '',
+    //TrackList/TrackColumn headings (excluding Data type/class)
+    '#':     '',
+    'Title': '',
+    //Filter[.UI] strings
+    'Filter'      : '',
+    'Filters'     : '',
+    'QuickFilter' : '',
+    'Options'     : '',
+    'Negate'      : '',
+        //Long type strings
+        'Artist Filter'   : '',
+        'Genre Filter'    : '',
+        'Label Filter'    : '',
+        'Release Filter'  : '',
+        'Role Filter'     : '',
+        'Tag Filter'      : '',
+        'Regex Filter'    : '',
+        'Compound Filter' : '',
+        //Short type strings (excluding Data type/class)
+        'Regex'    : '',
+        'Compound' : '',
+        //Data
+        'Recursive': '',
+            //Placeholder text
+            'Artist list loading'  : '',
+            'Genre list loading'   : '',
+            'Label list loading'   : '',
+            'Release list loading' : '',
+            'Role list loading'    : '',
+            'Tag list loading'     : '',
+        //Regex
+        'Case Sensitive' : '',
+        'Enter regex'    : '',
+        //Compound
+        'Operator'     : '',
+        'Or'           : '',
+        'And'          : '',
+        'Xor'          : '',
+        'New Filter'   : '',
+        'Empty Filter' : '',
+    //Some generic predefined Role types
+    'Producer' : '',
+    'Remixer'  : '',
+    'Vocals'   : '',
+    //Misc.
+    'Play'        : '',
+    'Edit'        : '',
+    'Unknown'     : '',
+    'Loading'     : '',
+    'Remove'      : '',
+    'Search'      : '',
+    'Shuffle'     : '',
+    'Shuffle Off' : '',
+    'Shuffle On'  : '',
+});
+*/
