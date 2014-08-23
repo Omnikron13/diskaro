d6ff4c3710c84667cc07b663ba9786f20e60a87d
Joey Sabey <GameFreak7744@gmail.com>
2014-08-23 20:15:50 +0100

    Updated Data.UI.LI to use the new Data.UI.render() method to create its base element

--- js/Data.UI.js
+++ js/Data.UI.js
@@ -20,15 +20,10 @@ Data.UI = {
     LI: {
         //Function to render <li> (jQuery) element from Data obj
         render: function(d) {
-            return $('<li>')
-                //Store original Data obj
-                .data('data', d)
-                //Add generic selection class
+            //Create base Data.UI element
+            return Data.UI.render(d, 'li')
+                //Add more specific selection class
                 .addClass('dataItem')
-                //Add specific selecion class (e.g. .dataItem.Genre)
-                .addClass(d.type)
-                //Render output text/html
-                .html(d.name)
             ;
         },
     },
