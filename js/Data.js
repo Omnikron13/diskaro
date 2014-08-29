a566d597199413d66d7855da591739af1cf971d2
Joey Sabey <GameFreak7744@gmail.com>
2014-08-29 17:55:41 +0100

    Added .addParent() & .removeParent() methods to Data - both accept another Data obj which (if it matches in type & doesn't present an integrity clash of some sort) is used to add/remove an id from the parentIDs array, and add/remove the corresponding id from the given Data objects childIDs array. Also added complimentary .addChild()/.removeChild() methods which merely delegate to .addParent()/.removeParent() (though obviously with the given Data obj & this flipped)

--- js/Data.js
+++ js/Data.js
@@ -30,6 +30,54 @@ Data.method('clone', function() {
     return Data[this.type](JSON.parse(JSON.stringify(this)));
 });
 
+//Method to add parent(ID) link (& matching child(ID) link)
+Data.method('addParent', function(d) {
+    //Abort if types don't match
+    if(d.type != this.type) return this;
+    //Abort if trying to add this as a parent of itself
+    if(d.id == this.id) return this;
+    //Abort if given parent(ID) is already in array
+    if(this.parentIDs.indexOf(d.id) != -1) return this;
+    //Add new parent(ID) to this
+    this.parentIDs.push(d.id);
+    //Done if new parent already has child link
+    if(d.childIDs.indexOf(this.id) != -1) return this;
+    //Add corresponding child(ID) to new parent
+    d.childIDs.push(this.id);
+    //Return this (enable chaining)
+    return this;
+});
+
+//Method to remove parent(ID) link (& matching child(ID) link)
+Data.method('removeParent', function(d) {
+    //Abort if types don't match
+    if(d.type != this.type) return this;
+    //Get array index of given parent(ID)
+    var i = this.parentIDs.indexOf(d.id);
+    //Remove parent(ID) if exists
+    if(i != -1) this.parentIDs.splice(i, 1);
+    //Get array index of corresponding child link
+    var i = d.childIDs.indexOf(this.id);
+    //Remove child(ID) if exists
+    if(i != -1) d.childIDs.splice(i, 1);
+    //Return this (enable chaining)
+    return this;
+});
+
+//Method to add child(ID) link (& matching parent(ID) link)
+Data.method('addChild', function(d) {
+    //Delegate to .addParent() & return
+    d.addParent(this);
+    return this;
+});
+
+//Method to remove child(ID) link (& matching parent(ID) link)
+Data.method('removeChild', function(d) {
+    //Delegate to .removeParent() & return
+    d.removeParent(this);
+    return this;
+});
+
 //Method to update Data obj props 'in place' (won't break refs)
 Data.method('update', function(d) {
     //Abort update if type or id don't match
