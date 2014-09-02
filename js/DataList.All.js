b15f501061b633747717ee6183d6ed0b264579b1
Joey Sabey <GameFreak7744@gmail.com>
2014-09-02 22:42:31 +0100

    Added get/set Parents/Children methods to Data in DataList.All.js - getters & setters for the Data objects .parentIDs/.childIDs array & new .parents/.children cached DataList properties, accepting either id array or DataList, aborting if the Data type doesn't support parents/children

--- js/DataList.All.js
+++ js/DataList.All.js
@@ -31,6 +31,56 @@ DataList.All = {
 /*-------------------------*
  | Data class enhancements |
  *-------------------------*/
+//Getter method to return DataList of parents from .parentIDs
+Data.method('getParents', function() {
+    //Abort if Data obj doesn't support parents
+    if(!this.hasOwnProperty('parentIDs')) return null;
+    //If parents DataList isn't cached, convert .parentIDs
+    if(!this.parents)
+        this.parents = DataList.All[this.type].getSubset(this.parentIDs);
+    //Return (cached) parents DataList
+    return this.parents;
+});
+
+//Getter method to return DataList of children from .childIDs
+Data.method('getChildren', function() {
+    //Abort if Data obj doesn't support children
+    if(!this.hasOwnProperty('childIDs')) return null;
+    //If childs DataList isn't cached, convert .childIDs
+    if(!this.children)
+        this.children = DataList.All[this.type].getSubset(this.childIDs);
+    //Return (cached) children DataList
+    return this.children;
+});
+
+//Setter method to set parentIDs & cached DataList from id-array/DataList
+Data.method('setParents', function(dl) {
+    //Abort if Data obj doesn't support parents
+    if(!this.hasOwnProperty('parentIDs')) return null;
+    //If not passed a DataList assume id array & convert
+    if(!dl instanceof DataList)
+        dl = DataList.All[this.type].getSubset(dl);
+    //Set parent id array & (cached) DataList
+    this.parentIDs = dl.getIDs();
+    this.parents = dl;
+    //Enable chaining
+    return this;
+});
+
+//Setter method to set childIDs & cached DataList from id-array/DataList
+Data.method('setChildren', function(dl) {
+    //Abort if Data obj doesn't support children
+    if(!this.hasOwnProperty('childIDs')) return null;
+    //If not passed a DataList assume id array & convert
+    if(!dl instanceof DataList)
+        dl = DataList.All[this.type].getSubset(dl);
+    //Set child id array & (cached) DataList
+    this.childIDs = dl.getIDs();
+    this.children = dl;
+    //Enable chaining
+    return this;
+});
+
 //Method to convert parentIDs to DataList (accessed by callback)
 Data.method('getParentList', function(cb) {
     //Abort if Data obj doesn't support parents
