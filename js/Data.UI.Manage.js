1c08c869c42e8a99401cdd9cd0ea1928b54e9c17
Joey Sabey <GameFreak7744@gmail.com>
2014-09-03 17:50:13 +0100

    Added Data.UI.Manage.js - defines Data.UI.Manage namespace for UI functionality allowing the user to add/edit/remove Data ([Sub]DataCore) entries in the DB. Currently provides .render() function which renders a (DataList.UI) based element outputing all Data entries of a given type string (and in an optional given render 'style'). Currently only edit functionality is possible; add/remove needs adding. Currently there are two predefined output/render 'styles' in Data.UI.Manage.Style - .List() which renders out a DataList.UI.UL, and .Tree() which renders out a DataList.UI.Tree (not suitable for Data types which don't support parents/children, which is currently only Release). Default style used is .List for Release, .Tree for all others.

---
+++ js/Data.UI.Manage.js
@@ -0,0 +1,73 @@
+//require_once(Data.UI.Edit.js)
+//require_once(DataList.UI.Tree.js)
+
+//Namespace for functionality to add/edit/remove Data entries from the DB
+Data.UI.Manage = {
+    //Funtion to render UI element allowing the user to edit Data entries
+    render: function(type, opt) {
+        //Set default render style (Release can't render as a tree)
+        var style = type == 'Release' ? 'List' : 'Tree';
+        //Override defaults as appropriate
+        if(opt) {
+            if(opt.style) style = opt.style;
+        }
+        //Get DataList obj from type str
+        var dl = DataList.All[type];
+        //Render container element
+        return $('<div>')
+            //Add generic selection class
+            .addClass('manage')
+            //Add specific selection class (e.g. .manage.Genre)
+            .addClass(type)
+            //Render element of specified style
+            .append(
+                Data.UI.Manage.Style[style](dl)
+            )
+            //Catch Data.UI click events & open edit dialogue
+            .on('click', '.data', function(ev) {
+                //Save ref to DataList.UI element for closure(s)
+                var dlui = $(ev.delegateTarget).children('.dataList');
+                //Save ref to (master) Data obj for closure(s)
+                var d = $(this).triggerHandler('getData');
+                //Render Data edit dialogue on (cloned) Data obj
+                Data.UI.Edit.render(d.clone())
+                    //Catch (possibly) updated Data obj
+                    .on('save', function(ev, _d) {
+                        //Send update request to server
+                        Request.Update(type, _d)
+                            .process(function(response) {
+                                //Alert/abort on request failure
+                                if(!response.success) {
+                                    window.alert(response.message);
+                                    return;
+                                }
+                                //Update (master) Data obj
+                                d.update(Data[type](response.data));
+                                //Instruct all Data.UI of this type/id to update
+                                $(d.getSelectionClass())
+                                    .trigger('updateOutput')
+                                ;
+                                //Instruct DataList.UI to peform any necessary updates
+                                dlui.trigger('updateData', d);
+                            })
+                    })
+                ;
+            })
+        ;
+    },
+
+    //Namespace for predefined render styles
+    Style: {
+        //Style to render as a flat list
+        List: function(dl) {
+            return DataList.UI.UL.render(dl)
+            ;
+        },
+
+        //Style to render as a tree structure
+        Tree: function(dl) {
+            return DataList.UI.Tree[dl.type]()
+            ;
+        },
+    },
+};
