568d265a5dd57342446c703aac5e1e8e567987a7
Joey Sabey <GameFreak7744@gmail.com>
2014-09-07 21:12:18 +0100

    Added Filter.UI.Year.js which defines new Filter.UI.Year namespace - preliminary codebase for rendering Filter.Year UI elements. Basic logic is working ok, though currently changes to the end year field can cause invalid year filter states (e.g. start=2004, end=1999). When in a 'quickfilter' an invalid state means the tracklist won't update until the filter is back into a valid state, which is ok (failing silently is always at least ok ;)) but it might be better if the UI indicated the invalid state, and perhaps invalid end year entry should be treated as NULL entry (i.e. the year filter will revert to exact matches against the start year)

---
+++ js/Filter.UI.Year.js
@@ -0,0 +1,125 @@
+//require_once(Filter.UI.js)
+
+//Namespace for Filter.Year UI elements
+Filter.UI.Year = {
+    //Default UI strings
+    yearsHeader      : _('Start/End Years'),
+    startLabel       : _('Start'),
+    startPlaceholder : _('Start') + '...',
+    endLabel         : _('End'),
+    endPlaceholder   : _('End') + '...',
+
+    //Function for creating YearFilter/Filter.Year UI elements
+    render: function(prefix) {
+        //Create element from base template
+        return Filter.UI.core.render(
+            'Year',
+            null,
+            prefix
+        )
+            //Add year specific UI fields/controls
+            .append(
+                $('<fieldset>')
+                    //Add class for generic selection (e.g. .filter>.body)
+                    .addClass('body')
+                    //Add class for specific selection (e.g. .yearFilter>.body.years)
+                    .addClass('years')
+                    //Render section header element
+                    .append(
+                        $('<legend>')
+                            //Add class for generic selection (e.g. .filter>.body>.header)
+                            .addClass('header')
+                            .html(Filter.UI.Year.yearsHeader)
+                    )
+                    //Render start year input label
+                    .append(
+                        $('<label>')
+                            .attr('for', 'startField')
+                            .html(Filter.UI.Year.startLabel)
+                    )
+                    //Render start year input field
+                    .append(
+                        $('<input>')
+                            .addClass('start')
+                            .attr('name', 'startField')
+                            .attr('type', 'text')
+                            .attr('placeholder', Filter.UI.Year.startPlaceholder)
+                            //Catch /actual/ changes to the field
+                            .on('input', function() {
+                                var y = $(this).val();
+                                $(this).trigger('updateStart', Number.parseInt(y, 10) || null);
+                            })
+                    )
+                    //Render end year input label
+                    .append(
+                        $('<label>')
+                            .attr('for', 'endField')
+                            .html(Filter.UI.Year.endLabel)
+                    )
+                    //Render end year input field
+                    .append(
+                        $('<input>')
+                            .addClass('end')
+                            .attr('name', 'endField')
+                            .attr('type', 'text')
+                            .attr('placeholder', Filter.UI.Year.endPlaceholder)
+                            //Catch /actual/ changes to the field
+                            .on('input', function() {
+                                var y = $(this).val();
+                                $(this).trigger('updateEnd', Number.parseInt(y, 10) || null);
+                            })
+                    )
+            )
+
+            //Catch start year updates
+            .on('updateStart', function(ev, y) {
+                //Start year has been nulled
+                if(!y) {
+                    $(this)
+                        //Null stored Filter obj
+                        .removeData('filter')
+                        //Alert parents that Filter is nulled
+                        .trigger('filterUpdate', null)
+                    ;
+                    //Stop propagation
+                    return false;
+                }
+                //Get stored Filter obj
+                var f = $(this).data('filter');
+                //No filter stored; create one
+                if(!f) f = $(this).data(
+                    'filter',
+                    Filter.Year(
+                        y,
+                        $(this).data('end'),
+                        $(this).data('options')['Negate']
+                    )
+                ).data('filter');
+                //Filter obj was stored; update it
+                else f.start = y;
+                //Trigger filterUpdate to alert parents
+                $(this).trigger('filterUpdate', f);
+                //Stop propagation
+                return false;
+            })
+
+            //Catch end year updates
+            .on('updateEnd', function(ev, y) {
+                //Store new end year (or null it)
+                if(y) $(this).data('end', y);
+                else $(this).removeData('end');
+                //Get stored Filter obj
+                var f = $(this).data('filter');
+                //Abort if there isn't a Filter to update
+                if(!f) return false;
+                //Update Filter obj
+                f.end = y;
+                //Trigger filterUpdate to alert parents
+                $(this).trigger('filterUpdate', f);
+                //Stop propagation
+                return false;
+            })
+        ;
+    },
+};
+
